9803af5e567f8b3a1e7ab08c31f83febf1831b7b
Derby-Blessing <derbyatswei.kommey@studenti.unipr.it>
2021-09-12 16:37:23 +0200

    NewHome commit

--- src/components/pages/NewHome.js
+++ src/components/pages/NewHome.js
@@ -1,86 +1,75 @@
 import React from "react";
+import { Card, CardGroup, Button } from "react-bootstrap";
 
 class NewHome extends React.Component {
   render() {
     return (
       <>
         <center>
-          <div class="card mb-3 max-width: 540px;">
-            <div class="row no-gutters">
-              <div class="col-md-4">
-                <img src="Images/Consigliami.jpg" class="card-img"></img>
-              </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h2 class="card-title">Consigliami</h2>
-                  <p class="card-text">La pagina consigliami.....</p>
-                  <a href="#" class="btn btn-primary">
-                    Vedi
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="card-columns">
-            <div class="card">
-              <img
-                src="Images/Uni_scelta1.jpg"
-                class="card-img-top"
-                alt="..."
-              ></img>
-              <div class="card-body">
-                <h2 class="card-title">Trova Ateneo</h2>
-                <p class="card-text">
+          <CardGroup>
+            <Card>
+              <Card.Img variant="top" src="Imaggini/advice1.jpg/100px160" />
+              <Card.Body>
+                <Card.Title>Consigliami</Card.Title>
+                <Card.Text>La pagina consigliami.....</Card.Text>
+              </Card.Body>
+              <Button
+                variant="primary"
+                type="submit"
+                onClick={this.handleClick}
+              >
+                Vedi
+              </Button>
+            </Card>
+            <Card>
+              <Card.Img variant="top" src="Imaggini/trova_uni3.jpg/100px160" />
+              <Card.Body>
+                <Card.Title>Trova Ateneo</Card.Title>
+                <Card.Text>
                   Non sei sicura di quale ateneo vuoi frequentare? Sei indecisa
                   su quale corso è giusto per te? Non preoccuparti più....Siamo
                   qui per aiutarti.......
-                </p>
-                <a href="#" class="btn btn-primary">
-                  Vedi
-                </a>
-              </div>
-            </div>
-            <div class="card">
-              <img
-                src="Images/Reviews.jpg"
-                class="card-img-top"
-                alt="..."
-              ></img>
-              <div class="card-body">
-                <h2 class="card-title">Lasciaci Una Recensione</h2>
-                <p class="card-text">
+                </Card.Text>
+              </Card.Body>
+              <Button
+                variant="primary"
+                type="submit"
+                onClick={this.handleClick}
+              >
+                Vedi
+              </Button>
+            </Card>
+          </CardGroup>
+          <CardGroup>
+            <Card>
+              <Card.Img variant="top" src="Imaggini/recensisci3.jpg/100px160" />
+              <Card.Body>
+                <Card.Title>Lasciaci Una Recensione</Card.Title>
+                <Card.Text>
                   Hai avuta una buona esperienza con AIUniversity? Hai trovato
                   l'ateneo che volevi?
-                </p>
-                <a href="#" class="btn btn-primary">
-                  Vedi
-                </a>
-              </div>
-            </div>
-            <div class="card  width: 18rem;">
-              <img
-                src="Images/More_info.jpg"
-                class="card-img-top"
-                alt="..."
-              ></img>
-              <div class="card-body">
-                <h2 class="card-title">Hai Bisogno di Altri Informazioni?</h2>
-                <p class="card-text"></p>
-              </div>
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item">
-                  <a href="#" class="card-link">
-                    Trova Corso Di Laurea
-                  </a>
-                </li>
-                <li class="list-group-item">
-                  <a href="#" class="card-link">
-                    Trova Ateneo
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
+                </Card.Text>
+              </Card.Body>
+              <Button
+                variant="primary"
+                type="submit"
+                onClick={this.handleClick}
+              >
+                Vedi
+              </Button>
+            </Card>
+            <Card>
+              <Card.Img variant="top" src="Imaggini/cosigliami2.jpg/100px160" />
+              <Card.Body>
+                <Card.Title>Hai Bisogno di Altri Informazioni?</Card.Title>
+                <Card.Text>Hai Bisogno di Altri Informazioni?</Card.Text>
+              </Card.Body>
+              <Card.Body>
+                <Card.Link href="#">Trova Corso Di Laurea</Card.Link>
+                <Card.Link href="#">ATrova Ateneo</Card.Link>
+              </Card.Body>
+            </Card>
+          </CardGroup>
         </center>
       </>
     );
